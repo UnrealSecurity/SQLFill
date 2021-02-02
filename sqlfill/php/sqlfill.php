d253d0b45f28b8468d85243ce9be328013e05265
UnrealSec <arran.bishop89@aol.com>
2021-02-02 03:22:37 +0200

    Commit death

---
+++ sqlfill/php/sqlfill.php
@@ -0,0 +1,128 @@
+<?php 
+
+     /* Author @UnrealSec  */
+
+    class SQLFill {
+        function __construct($host, $user, $pass, $dbname, $port = 3306) {
+            try {
+                $this->queries = [];
+                $this->mysqli = new mysqli($host, $user, $pass, $dbname, $port);
+            } catch (Exception $err) {
+                
+            }
+        }
+
+        function fill($template, ...$data) {
+            $array = str_split($template);
+
+            $j = 0;
+            for ($i = 0; $i < count($array); $i++) {
+                if ($array[$i] === '?') {
+                    $value = $data[$j++];
+                    $d = SQLFill::get_array_d($value);
+
+                    if ($d === 0) {
+                        $array[$i] = SQLFill::escape($value);
+                    } else if ($d === 1) {
+                        $array[$i] = SQLFill::escape_array($value);
+                    } else if ($d === 2) {
+                        $array[$i] = SQLFill::escape_array_2d($value);
+                    }
+                } else if ($array[$i] === '$') {
+                    $value = $data[$j++];
+                    $d = SQLFill::get_array_d($value);
+
+                    if ($d === 0) {
+                        if (!SQLFill::is_valid_field_name($value)) {
+                            throw new Exception($this::$strings[0].$this::$strings[1]);
+                        }
+                        $array[$i] = SQLFill::escape($value, false);
+                    } else if ($d === 1) {
+                        foreach ($value as $v) {
+                            if (!SQLFill::is_valid_field_name($v)) {
+                                throw new Exception($this::$strings[0].$this::$strings[1]);
+                            }
+                        }
+                        $array[$i] = SQLFill::escape_array($value, false);
+                    } else if ($d === 2) {
+                        foreach ($value as $v1) {
+                            foreach ($v1 as $v2) {
+                                if (!SQLFill::is_valid_field_name($v2)) {
+                                    throw new Exception($this::$strings[0].$this::$strings[1]);
+                                }
+                            }
+                        }
+                        $array[$i] = SQLFill::escape_array_2d($value, false);
+                    }
+                }
+            }
+
+            $this->queries[] = implode('', $array);
+            return $this;
+        }
+
+        function tostr() {
+            return implode(';', $this->queries);
+        }
+
+        function flush() {
+            $this->queries = [];
+        }
+
+        function query() {
+            $query = $this->tostr();
+            $this->flush();
+            return new SQLFillResult($this, $this->mysqli->query($query));
+        }
+
+        static function escape($str, $quotes = true) {
+            $str = str_replace('\'', '\\\'', $str);
+            return $quotes ? "'".$str."'" : $str;
+        }
+
+        static function escape_array($array, $quotes = true) {
+            for ($i = 0; $i < count($array); $i++) {
+                $array[$i] = SQLFill::escape($array[$i], $quotes);
+            }
+            return "(".implode(',', $array).")";
+        }
+
+        static function escape_array_2d($array, $quotes = true) {
+            for ($i = 0; $i < count($array); $i++) {
+                $array[$i] = SQLFill::escape_array($array[$i], $quotes);
+            }
+            return implode(',', $array);
+        }
+
+        static function is_valid_field_name($name) {
+            return preg_match('/^.[A-z0-9_\.\(\)]*$/', $name) === 1;
+        }
+
+        static function get_array_d($array) {
+            if (is_array($array)) {
+                if (count($array) > 0 && is_array($array[0])) {
+                    return 2;
+                }
+                return 1;
+            }
+            return 0;
+        }
+
+        public static $strings = [
+            'SQLFill: ',
+            'Failed to fill database query!'
+        ];
+    }
+
+    class SQLFillResult {
+        function __construct($sqlfill, $result) {
+            $this->sqlfill = $sqlfill;
+            $this->result = $result;
+        }
+
+        function fetch() {
+            return $this->result->fetch_assoc();
+        }
+    }
+
+?>
